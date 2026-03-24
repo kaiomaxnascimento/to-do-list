66f44db0b2352b372124438abcc09d5caafa3b9f
Kaiomax Nascimento <kaiomaxnascimento@gmail.com>
2026-03-24 19:49:55 -0300

    js criado com funcao de adicionar, remover, cheked e salvando no localstorage

---
+++ script.js
@@ -0,0 +1,41 @@
+const input = document.getElementById("input-text");
+const lista = document.querySelector(".lista");
+const btn = document.querySelector(".button");
+
+function generateList() {
+  if (input.value === "") {
+    alert("Digite Algo para adicionar a lista");
+  } else {
+    const element = document.createElement("li");
+    const span = document.createElement("span");
+    element.innerText = input.value;
+    span.classList.add("remove");
+    lista.appendChild(element);
+    element.appendChild(span);
+    input.value = "";
+    saveData();
+    input.focus();
+  }
+}
+
+function chekedList(event) {
+  if (event.target.tagName === "LI") {
+    event.target.classList.toggle("checked");
+    saveData();
+  } else if (event.target.tagName === "SPAN") {
+    event.target.parentElement.remove("remove");
+    saveData();
+  }
+}
+
+btn.addEventListener("click", generateList);
+lista.addEventListener("click", chekedList);
+
+function saveData() {
+  localStorage.setItem("data", lista.innerHTML);
+}
+
+function showList() {
+  lista.innerHTML = localStorage.getItem("data");
+}
+showList();
