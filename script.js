687e743e6b5d66ec81dce8702286d0ded3e7cca5
Kaiomax Nascimento <kaiomaxnascimento@gmail.com>
2026-03-26 23:19:13 -0300

    adicionado nivel de progresso de tarefas concluidas

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const input = document.getElementById("input-text");
 const lista = document.querySelector(".lista");
 const btn = document.querySelector(".button");
+const textProgresso = document.querySelector(".progresso");
 
 function generateList() {
   if (input.value === "") {
@@ -13,6 +14,7 @@ function generateList() {
     lista.appendChild(element);
     element.appendChild(span);
     input.value = "";
+    progress();
     saveData();
     input.focus();
   }
@@ -21,9 +23,11 @@ function generateList() {
 function chekedList(event) {
   if (event.target.tagName === "LI") {
     event.target.classList.toggle("checked");
+    progress();
     saveData();
   } else if (event.target.classList.contains("remove")) {
     event.target.parentElement.remove("remove");
+    progress();
     saveData();
   }
 }
@@ -31,7 +35,11 @@ function chekedList(event) {
 btn.addEventListener("click", generateList);
 lista.addEventListener("click", chekedList);
 input.addEventListener("keydown", (event) => {
-  if (event.key === "Enter") generateList();
+  if (event.key === "Enter") {
+    event.preventDefault();
+    generateList();
+    progress();
+  }
 });
 
 function saveData() {
@@ -42,3 +50,20 @@ function showList() {
   lista.innerHTML = localStorage.getItem("data");
 }
 showList();
+
+function progress() {
+  const total = Array.from(lista.children);
+  const completed = document.querySelector(".progresso-cont");
+  let checked = 0;
+  total.filter((checkado) => {
+    if (checkado.classList.contains("checked") === true) checked++;
+    textProgresso.innerText = `${checked} / ${total.length}`;
+  });
+  console.log(checked);
+  if (checked === total.length) {
+    completed.classList.add("full");
+  } else {
+    completed.classList.remove("full");
+  }
+}
+progress();
